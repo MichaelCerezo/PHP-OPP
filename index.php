e1d37a94e80ca1d7731beb0ded211bdbcb23a293
Michael Cerezo <cerezo.michael@gmail.com>
2015-01-07 10:44:58 -0800

    Dans lecture

--- index.php
+++ index.php
@@ -28,5 +28,22 @@
 	//-----------------------------------------------------------------------------------------
 	// Example 3 
 	//-----------------------------------------------------------------------------------------
- 	
+ 	class car{
+		public $name = "default name";
+		public $type = "toyota";
+		public $doors = "4";
+		public $price = "500";	
+	}
+	class house{
+		public $name = "default name";
+		public $type = "apartments";
+		public $floors = "2";
+		public $price = "1500";
+	}
+	class food{
+		public $name = "default name";
+		public $type = "Mexican";
+		public $dish = "tacos";
+		public $price = "1.50";
+	}
  ?>
